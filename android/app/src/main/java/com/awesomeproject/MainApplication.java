66e977ae6f4774138f08b31a1d6dc8c6495796a6
AhmedKhchai <a.khchai@gmail.com>
2023-06-21 18:43:54 +0200

    typo

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -22,7 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-                    new ARPackage() // Add this line
+                    new ARPackage() 
             );
         }
 
