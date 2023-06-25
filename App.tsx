95472cf794d6c0dde97aac9a54c4b1c20561e053
AhmedKhchai <a.khchai@gmail.com>
2023-06-25 19:08:05 +0200

    to be debuged

--- App.tsx
+++ App.tsx
@@ -1,41 +1,20 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Button,
-  NativeModules,
-} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import LoginScreen from './screens/LoginScreen';
 
-function App() {
-  const openARViewer = () => {
-    const fileUrl =
-      'https://modelviewer.dev/shared-assets/models/Astronaut.glb'; // Replace with your actual 3D model URL
-    const title = 'Astronaut'; // Replace with your actual title
-    NativeModules.ARModule.openViewer(fileUrl, title);
-  };
+const Stack = createStackNavigator();
+const queryClient = new QueryClient();
 
+export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.inner}>
-        <Button title="Open AR Viewer" onPress={openARViewer} />
-      </View>
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="LoginScreen">
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  inner: {
-    width: '100%',
-    maxWidth: 400,
-    alignSelf: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-});
-
-export default App;
